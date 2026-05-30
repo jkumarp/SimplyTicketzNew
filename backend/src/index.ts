75c5b0be651adb2590d5afc0c3703b81f7e455ed
[dyad] <japillai@gmail.com>
2026-05-31 00:43:05 +0530

    [dyad] Adding getStates API endpoint with country filtering - wrote 3 file(s)

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,7 @@ import userRoutes from './routes/userRoutes';
 import merchantRoutes from './routes/merchantRoutes';
 import documentRoutes from './routes/documentRoutes';
 import countryRoutes from './routes/countryRoutes';
+import stateRoutes from './routes/stateRoutes';
 
 
 const app = express();
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use('/api', userRoutes);
 app.use('/api', merchantRoutes);
 app.use('/api', countryRoutes);
+app.use('/api', stateRoutes);
 app.use('/api/documents', documentRoutes);
 
 
