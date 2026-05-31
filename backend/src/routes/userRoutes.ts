5dad96a5ee31f493c49d828999460e429726292d
[dyad] <japillai@gmail.com>
2026-05-31 16:52:43 +0530

    [dyad] Implementing JWE-based authentication and login UI - wrote 5 file(s), added jose package(s)

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
-import { getUsers, createUser } from '../controllers/userController';
+import { getUsers, createUser, signInUser, signOutUser } from '../controllers/userController';
 
 const router = Router();
 
 router.get('/users', getUsers);
 router.post('/users', createUser);
+router.post('/login', signInUser);
+router.post('/logout', signOutUser);
 
 export default router;
