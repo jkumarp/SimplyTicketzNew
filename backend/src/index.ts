f7d5d2a08b7056a77cc00e02a86b99e6cb395073
[dyad] <japillai@gmail.com>
2026-06-04 23:11:58 +0530

    [dyad] Adding routes for merchant device management - wrote 2 file(s)

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,6 +8,7 @@ import merchantServicesRoutes from './routes/merchantServicesRoutes';
 import merchantSubscriptionRoutes from './routes/merchantSubscriptionRoutes';
 import ticketTimeslotRoutes from './routes/ticketTimeslotRoutes';
 import ticketCategoryRoutes from './routes/ticketCategoryRoutes';
+import merchantDeviceRoutes from './routes/merchantDeviceRoutes';
 import documentRoutes from './routes/documentRoutes';
 import countryRoutes from './routes/countryRoutes';
 import stateRoutes from './routes/stateRoutes';
@@ -44,6 +45,7 @@ app.use('/api', authorizeRoles([1, 2]), merchantServicesRoutes);
 app.use('/api', authorizeRoles([1, 2]), merchantSubscriptionRoutes);
 app.use('/api', authorizeRoles([1, 2]), ticketTimeslotRoutes);
 app.use('/api', authorizeRoles([1, 2]), ticketCategoryRoutes);
+app.use('/api', authorizeRoles([1, 2]), merchantDeviceRoutes);
 
 // Other master data routes
 app.use('/api', countryRoutes);
